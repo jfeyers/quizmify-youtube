2564cba3002632d802a3f1149c8bc415bdd50507
Jason Feyers <jason_feyers@hermanmiller.com>
2023-09-14 10:43:44 -0400

    hardcoded env

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -42,7 +42,7 @@ export async function POST(req: Request, res: Response) {
     });
 
     const { data } = await axios.post(
-      `${process.env.API_URL as string}/api/questions`,
+      `https://quizmify-youtube-nine.vercel.app/api/questions`,
       {
         amount,
         topic,
