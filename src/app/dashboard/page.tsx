4e1774be63a74160e4e12b4410290368cad62676
Jason Feyers <jason_feyers@hermanmiller.com>
2023-09-14 12:22:52 -0400

    revert back

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -11,13 +11,23 @@ export const metadata = {
   title: "Dashboard | Quizmify",
 };
 const Dashboard = async (props: Props) => {
+  const session = await getAuthSession();
+  if (!session?.user) {
+    return redirect("/");
+  }
   return (
     <main className="p-8 mx-auto max-w-7x1">
       <div className="flex items-center">
         <h2 className="mr-2 text-3x1 font-bold tracking-tight">Dashboard</h2>
       </div>
-      <div className="grid gap-4 mt-4 md:grid-cols-2">Test</div>
-      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">Test</div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2">
+        <QuizMeCard />
+        <HistoryCard />
+      </div>
+      <div className="grid gap-4 mt-4 md:grid-cols-2 lg:grid-cols-7">
+        <HotTopicsCard />
+        <RecentActivityCard />
+      </div>
     </main>
   );
 };
